0f2edbc58e971c1931c3ae29cc4270c880a85419
kerwin <59557132+898269703@users.noreply.github.com>
2026-09-08 14:54:06 +0800

    test(scrapling): reject non-html assets from recursive crawl

--- scrapling-worker/tests/test_spider_logic.py
+++ scrapling-worker/tests/test_spider_logic.py
@@ -45,6 +45,19 @@ def test_html_links_are_followed_within_depth():
     )
 
 
+def test_non_html_assets_are_not_recursively_followed():
+    for suffix in (".dtd", ".ent", ".cat", ".txt", ".zip", ".tgz", ".ps", ".css", ".js", ".xml"):
+        assert not should_follow(
+            f"https://www.w3.org/TR/REC-html40-971218/resource{suffix}",
+            allowed_hosts={"www.w3.org"},
+            depth=1,
+            max_depth=4,
+            include_patterns=[],
+            exclude_patterns=[],
+            scope_url="https://www.w3.org/TR/REC-html40-971218/",
+        )
+
+
 def test_external_or_too_deep_links_are_not_followed():
     assert not should_follow(
         "https://example.com/page",
